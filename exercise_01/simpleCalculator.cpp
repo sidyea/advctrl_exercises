7227e5472ff224a9d5d81cb9da4922c92659c295
sidyea <siddhant.dhanrajani@protonmail.com>
2022-11-16 15:37:17 +0100

    Added multiple-divide branch

--- exercise_01/simpleCalculator.cpp
+++ exercise_01/simpleCalculator.cpp
@@ -12,6 +12,18 @@ double subtract(double a, double b){
     return a - b;
 }
 
+double multiply(double a, double b){
+    return a * b;
+}
+
+double divide(double a, double b){
+    if(b != 0){
+        return a / b;
+    }else{
+        cout << "b has value " << b << "!\n"; //should print val 0.0
+    }
+}
+
 int main(){
 
     double a,b;
