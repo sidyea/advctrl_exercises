447909a709e8c8bc599978975a6ab2cd72bbf893
sidyea <siddhant.dhanrajani@protonmail.com>
2022-11-16 15:18:32 +0100

    added sum-subtract branch

--- exercise_01/simpleCalculator.cpp
+++ exercise_01/simpleCalculator.cpp
@@ -2,7 +2,25 @@
 
 using namespace std;
 
+double sum(double a,double b){
+    
+    return a + b;
+}
+
+double subtract(double a, double b){
+
+    return a - b;
+}
+
 int main(){
+
+    double a,b;
+    double sum, subtract;
+
+    cout << "input a: ";
+    cin >> a;
+    cout << "\ninput b: ";
+    cin >> b;
 
     return 0;
 }
