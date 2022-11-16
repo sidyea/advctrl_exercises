7ae83b94992c07f6932cd8091df2085e849e6f8b
sidyea <siddhant.dhanrajani@protonmail.com>
2022-11-16 16:07:44 +0100

    Implemented menu functionality

--- exercise_01/simpleCalculator.cpp
+++ exercise_01/simpleCalculator.cpp
@@ -20,7 +20,7 @@ double divide(double a, double b){
     if(b != 0){
         return a / b;
     }else{
-        cout << "b has value " << b << "!\n"; //should print val 0.0
+        cout << "b has value " << b << ". Should be nonzero!\n"; //should print val 0.0
     }
 }
 
@@ -31,12 +31,35 @@ double mean(double a, double b){
 int main(){
 
     double a,b;
-    double sum, subtract;
+    char op;
 
     cout << "input a: ";
-    cin >> a;
+        cin >> a;
     cout << "\ninput b: ";
-    cin >> b;
+        cin >> b;
+    
+    cout << "\n Menu: +,-,/,*,m \n Input symbol for operation: ";
+        cin >> op;
+
+    if(op == '+'){ //in c++ " " define a string literal and ' ' define a char literal
+        cout << "Sum is " << sum(a,b) << "\n";
+    }
+
+    if(op == '-'){
+        cout << "Difference is " << subtract(a,b) << "\n";
+    }
+
+    if(op == '/'){
+        cout << "Ratio is " << multiply(a,b) << "\n";
+    }
+
+    if(op == '*'){
+        cout << "Product is " << divide(a,b) << "\n";
+    }
+
+    if (op == 'm'){
+        cout << "Mean is " << mean(a,b) << "\n";
+    }
 
     return 0;
 }
